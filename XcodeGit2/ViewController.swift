35701c22d45060ef5147a3495f61f78c909cdd18
samurai <lastsamurai24@icloud.com>
2023-08-27 14:59:23 +0900

    二回目のプッシュ

--- XcodeGit2/ViewController.swift
+++ XcodeGit2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("初めてのコミット")
+        print("二回目のプッシュ")
         
         
     }
